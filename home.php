cccd97b433c75c63c7b2d767703a8d0870575efa
Farhana Bente Islam <21303195@iubat.edu>
2024-12-30 02:22:40 +0600

    Create home.php

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+
+require('./assets/header.php');
+?>
+
+  <section class="home-section">
+        <div class="home-img">
+            <img src="images/bank-banner.jpg" alt="">
+        </div>
+
+        <div class="home-content">
+            <img src="images/bank.jpg" alt="">
+            <p>WELCOME TO ABC BANK LTD</p>
+            <h2 class="fw-bold display-6 mb-3 py-3">Bank Customer Management System</h2>
+            <a href="user-login.php" class="btn">Login</a>
+  </section>
+<?php
+  require('./assets/footer.php');
+?>
